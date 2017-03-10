9b8b4f8a05f9fff6a836a837720ac1e6611a266b
David Silaban <david@infotech.co.id>
2017-03-10 19:16:41 +0700

    Use CMake build system

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,11 +1,30 @@
-
 LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libmidifile
+
+LOCAL_SRC_FILES = \
+    src-library/Binasc.cpp \
+    src-library/MidiEvent.cpp \
+    src-library/MidiEventList.cpp \
+    src-library/MidiFile.cpp \
+    src-library/MidiMessage.cpp \
+    src-library/Options.cpp
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+
+LOCAL_CFLAGS += -c -g -Wall -O3 -std=c++11
+
+include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := synth
 
-LOCAL_SRC_FILES := main.cpp
+LOCAL_SRC_FILES := synth.cpp midifile.cpp
+
+LOCAL_STATIC_LIBRARIES := libmidifile
 
 LOCAL_LDLIBS := -llog
 
